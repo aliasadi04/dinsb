0fff48f6659b124a547a5e50b43009aa112f27b9
goorekt <asadiali008@gmail.com>
2023-05-05 14:44:05 +0200

    responsive faq component + installed moment js

--- src/components/faq.component.tsx
+++ src/components/faq.component.tsx
@@ -17,21 +17,25 @@ const FAQ = ({ q, a }: { q: string, a: string }) => {
 
     return (
         <Box onClick={handleChange} sx={{
-      
-            mx: 5, borderColor: '#EE7203', borderWidth: 3, borderStyle: 'solid', my: 2, borderRadius: 5, width: '70%', px: 2, 
-            cursor:'pointer',
+
+            mx: 5, borderColor: '#EE7203', borderWidth: 3, borderStyle: 'solid', my: 2, borderRadius: 5, width: '70%', px: 2,
+            cursor: 'pointer',
             "&:hover": {
                 boxShadow: '0 5px 15px rgba(0,0,0,0.3)',
-                transition: 'box-shadow 0.1s ease-in-out',
-                
+                transition: 'box-shadow 0.2s ease-in-out',
+
 
             }, display: 'flex', flexDirection: 'column', alignItems: 'center', py: 2
         }}>
-            <Collapse in={open} collapsedSize={40} sx={{width:'100%'}} >
+            <Collapse in={open} collapsedSize={40} sx={{ width: '100%' }} >
 
                 <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }} >
-                    <Typography variant='h4' fontWeight={600}>{q}</Typography>
-                    <ArrowBackIosNewIcon fontSize='large' color='primary'sx={{ transform: (open ? 'rotate(270deg)' : 'rotate(90deg)') }} />
+                    <Typography
+                        fontSize={{ xs: 15, md: 25 }}
+                        // variant='h5' 
+
+                        fontWeight={600} sx={{ pb: 3 }}>{q}</Typography>
+                    <ArrowBackIosNewIcon fontSize='large' color='primary' sx={{ transform: (open ? 'rotate(270deg)' : 'rotate(90deg)'),transition:'transform 0.2s ease-in-out' }} />
                 </Box>
                 <Typography variant='h5' mt={4} >{a}</Typography>
 
