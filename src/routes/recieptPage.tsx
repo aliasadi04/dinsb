ebb8a6590b3b8a2ae3949b60fe0942610e39ec96
goorekt <asadiali008@gmail.com>
2023-06-05 00:07:16 +0200

    kvittering tabel færdigt

--- src/routes/recieptPage.tsx
+++ src/routes/recieptPage.tsx
@@ -1,39 +1,110 @@
-import { Box, Typography } from '@mui/material'
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { selectCurrentUser } from '../store/user/user.selector'
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import {
+	Box,
+	Paper,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
+	Typography,
+} from "@mui/material";
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectCurrentUser } from "../store/user/user.selector";
+import moment from "moment";
+
+const tableHeaderStyles = {
+	borderRight: "3px solid  white",
+	fontWeight: 700,
+	justifyContent: "center",
+	fontSize: { xs: 15, md: 20 },
+	color: "white",
+	textAlign: "center",
+};
+
+const tableCellStyle = {
+	border: "3px solid  white",
+	color: "white",
+	fontSize: { xs: 15, md: 20 },
+	textAlign: "center",
+};
 
 const RecieptPage = () => {
+	const currentUser = useSelector(selectCurrentUser);
 
-    const currentUser = useSelector(selectCurrentUser);
-    console.log(currentUser);
+	console.log(currentUser);
 
-    return (
-        <Box sx={{ px: 4, }}>
-            <Typography
-                variant='h2'
-                fontWeight={600} mx={4}>
-                Tak fordi du valgte os!</Typography>
+	return (
+		<Box
+			sx={{
+				px: 4,
+				display: "flex",
+				flexDirection: "column",
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<Typography
+				variant="h2"
+				fontWeight={600}
+				mx={4}
+				my={4}
+				textAlign={"center"}
+			>
+				Tak fordi du valgte os!
+			</Typography>
 
-            {currentUser.bookings &&
-                <table>
-                    <th>
-                        <td>Dage</td>
-                        <td>Antal dage</td>
-                        <td>Pris</td>
-                    </th>
-                    {currentUser.bookings.map(booking => <tr>
-                        <td>{booking.chosenDays}</td>
-                    </tr>)}
-                </table>
-            }
-            <Typography>
+			<Typography textAlign={"center"} mb={3} fontSize={{xs:20,md:25}}>
+				Soundboksen kan hentes ved Triumfvej 62B fra kl.9 til kl.22 på dagen
+				lejeperioden starter fra.
+			</Typography>
+			<Typography variant="h3" mb={1}>
+				Reserverationer
+			</Typography>
+			{currentUser && currentUser.bookings && (
+				<>
+					<TableContainer component={Paper} sx={{ maxWidth: 1000 }}>
+						<Table
+							sx={{ overflow: "scroll", bgcolor: "rgba(238, 114, 3,0.8)" }}
+						>
+							<TableHead>
+								<TableRow sx={{ border: "3px solid  white" }}>
+									<TableCell sx={tableHeaderStyles}>Id</TableCell>
+									<TableCell sx={tableHeaderStyles}>Antal dage</TableCell>
+									<TableCell sx={tableHeaderStyles}>Pris</TableCell>
+									<TableCell sx={tableHeaderStyles}>Dage</TableCell>
+								</TableRow>
+							</TableHead>
 
-            </Typography>
+							<TableBody>
+								{currentUser.bookings.map((booking, index) => (
+									<TableRow>
+										<TableCell sx={tableCellStyle}>{index + 1}</TableCell>
+										<TableCell sx={tableCellStyle}>
+											{booking.daysInterval}
+										</TableCell>
+										<TableCell sx={tableCellStyle}>{booking.pris}</TableCell>
+										<TableCell sx={tableCellStyle}>
+											{booking.chosenDays
+												.map((date) =>
+													moment(date, "MMM Do YY").format("DD/MM")
+												)
+												.join("-")}
+										</TableCell>
+									</TableRow>
+								))}
+							</TableBody>
+						</Table>
+					</TableContainer>
+				</>
+			)}
+			<Typography textAlign={"center"} fontStyle={"italic"} my={3} fontSize={{xs:15,md:23}}>
+				Passer prisen ikke? Vil du have levering? Kontakt os på +45 23 43 84 33,
+				så kan vi hjælpe dig hen ad vejen!
+			</Typography>
+		</Box>
+	);
+};
 
-        </Box>
-    )
-}
-
-export default RecieptPage
+export default RecieptPage;
